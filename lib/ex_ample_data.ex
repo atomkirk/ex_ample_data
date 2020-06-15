55b3c49b472543d6a423a63cbeb16642e09f672f
Adam Kirk <atomkirk@gmail.com>
2020-06-15 16:42:10 -0600

    init

---
+++ lib/ex_ample_data.ex
@@ -0,0 +1,18 @@
+defmodule ExAmpleData do
+  @moduledoc """
+  Documentation for `ExAmpleData`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAmpleData.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_ample_data_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAmpleDataTest do
+  use ExUnit.Case
+  doctest ExAmpleData
+
+  test "greets the world" do
+    assert ExAmpleData.hello() == :world
+  end
+end
